ba22097b259e0729ff52e27c0cd48b1c7dcb087c
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2024-01-25 10:42:17 +0500

    Start project

---
+++ Weather App/WeatherViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherViewController.swift
+//  Weather App
+//
+//  Created by Maksim  on 24.01.2024.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addLocationButtonTapped() {}
+    
+    @IBAction func locationButtonTapped() {}
+    
+
+
+}
+
